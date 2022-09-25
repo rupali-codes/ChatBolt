fecda2ec6da3a7210d55452b38021597eb73f566
Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
2022-09-25 16:32:04 +0530

    updating connection string

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_DB_URI, {useNewUrlParser: true})
 	.then(res => {
 		console.log("connected to database")
 	})
